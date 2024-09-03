fc6d3bad5fa572194309584e674567b9d7d293cd
iren-pd <irenpd45@gmail.com>
2024-09-03 23:03:45 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import PostService from './API/PostService';
+import { PostFilter } from './components/PostFilter';
+import { PostForm } from './components/PostForm';
+import { PostList } from './components/PostList';
+import { MyButton } from './components/UI/button/MyButton';
+import { Loader } from './components/UI/Loader/Loader';
+import { MyModal } from './components/UI/MyModal/MyModal';
+import { usePosts } from './hooks/usePosts';
+import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([]);
+    const [filter, setFilter] = useState({ sort: '', query: '' });
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost]);
+        setModal(false);
+    };
+
+    async function fetchPosts() {
+        setIsPostsLoading(true);
+        const posts = await PostService.getAll();
+        setPosts(posts);
+        setIsPostsLoading(false);
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter((p) => p.id !== post.id));
+    };
+
+    return (
+        <div className="App">
+            <MyButton
+                style={{ marginTop: '30px' }}
+                onClick={() => setModal(true)}
+            >
+                Создать пост
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost} />
+            </MyModal>
+
+            <hr style={{ margin: '15px 0' }} />
+            <PostFilter filter={filter} setFilter={setFilter} />
+            {isPostsLoading ? (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: '50px',
+                    }}
+                >
+                    <Loader />
+                </div>
+            ) : (
+                <PostList
+                    remove={removePost}
+                    posts={sortedAndSearchedPosts}
+                    title="Посты про JS"
+                />
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MyButton } from './UI/button/MyButton';
+
+export const PostItem = (props) => {
+    return (
+        <div className="post">
+            <div className=".post__constent">
+                <strong>
+                    {props.number}. {props.post.title}
+                </strong>
+                <div>{props.post.body}</div>
+            </div>
+            <div className="post__btns">
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Удалить
+                </MyButton>
+            </div>
+        </div>
+    );
+};
